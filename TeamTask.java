17f65e054815293833fd3bfb35bf4bddb123e348
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2022-03-02 11:09:31 -0500

    Started interface

---
+++ TeamTask.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+interface TeamTask {
+
+    double mean();
+    double mode();
+    double median();
+    double max();
+    double min();
+    double range();
+    boolean search();
+    //Sorting can be ascending and descending
+    ArrayList<Double> sortAsc();
+    ArrayList<Double> bubbleSort();
+    ArrayList<Double> selectionSort();
+    ArrayList<Double> insertionSort();
+    ArrayList<Double> quickSort();
+    ArrayList<Double> mergeSort();
+    ArrayList<Double> heapSort();
+    double factorial();
+    double sum();
+    
+
+
+}
