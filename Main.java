20483d748e25b1841e28252b9c8b66450d8c5e18
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2022-03-02 11:30:52 -0500

    Started main

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Double> list = new ArrayList<Double>();
+        boolean exit = false;
+
+    }
+    
+}
