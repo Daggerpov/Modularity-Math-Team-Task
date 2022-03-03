3144448a1d1d072a299dee5ade1ce10de7f218d1
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2022-03-03 09:09:31 -0500

    Started main, setup object

---
+++ Calculations.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Calculations {
+
+    private ArrayList<Double> list;
+
+    public Calculations(ArrayList<Double> l) {
+        list = l;
+    }
+
+    public ArrayList<Double> getList() {
+        return list;
+    }
+    
+}
--- Main.java
+++ Main.java
@@ -8,7 +8,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         ArrayList<Double> list = new ArrayList<Double>();
-        boolean exit = false;
+        boolean exit = true;
+
+        while (exit) {
+            System.out.print("Please enter a number. Type '0' to stop: ");
+            double num = sc.nextDouble();
+            if (num == 0) {
+                break;
+            } else {
+                list.add(num);
+            }
+        }
+
+        Calculations calculations = new Calculations(list);
+
+        System.out.println(calculations.getList());
+
+        sc.close();
 
     }
     
