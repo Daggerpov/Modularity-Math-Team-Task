89954a3d78a8d456357b1b1a9be2d7964dd2cb65
Daniel Agapov <53918934+Daggerpov@users.noreply.github.com>
2022-03-04 16:20:58 +0000

    fix main file conflicting changes text

--- Calculations.java
+++ Calculations.java
@@ -158,17 +158,16 @@ public class Calculations implements TeamTask {
 	
     //Daniel does these:
     public ArrayList<Double> bubbleSort() {
-		// int n = arr.length;
-  //       for (int i = 0; i < n-1; i++)
-  //           for (int j = 0; j < n-i-1; j++)
-  //               if (arr[j] > arr[j+1])
-  //               {
-  //                   // swap arr[j+1] and arr[j]
-  //                   int temp = arr[j];
-  //                   arr[j] = arr[j+1];
-  //                   arr[j+1] = temp;
-  //               }
-        
+		int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
 		return arrayToArrayList(arr);
     }
     public ArrayList<Double> selectionSort() {
